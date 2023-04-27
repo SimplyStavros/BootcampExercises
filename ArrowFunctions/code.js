15feac627527e2892fee759d34b0d44e0ab6c800
Alex <stavrosalexander13@gmail.com>
2023-04-27 16:43:28 -0400

    adding everything I should have before

---
+++ ArrowFunctions/code.js
@@ -0,0 +1,19 @@
+// function double(arr) {
+//     return arr.map(function(val) {
+//       return val * 2;
+//     });
+//   }
+//turn above code into one line with arrow functions
+let double = (arr => arr.map(val => val * 2));
+
+//   function squareAndFindEvens(numbers){
+//     var squares = numbers.map(function(num){
+//       return num ** 2;
+//     });
+//     var evens = squares.filter(function(square){
+//       return square % 2 === 0;
+//     });
+//     return evens;
+//   }
+//   turn all functions above into arrow functions
+const squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0);
